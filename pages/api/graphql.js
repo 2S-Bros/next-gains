1f22750f8438ab3910e5f015440e0ab9929463e8
EricSSartorius <EricSSartorius@gmail.com>
2019-11-04 10:23:30 -0700

    updated WIP

---
+++ pages/api/graphql.js
@@ -0,0 +1,34 @@
+import { ApolloServer } from "apollo-server-micro"
+import { mergeResolvers, mergeTypeDefs } from "graphql-toolkit"
+import connectDb from "../../lib/mongoose"
+import { exercisesResolvers } from "../../src/api/exercises/resolvers"
+import { exercisesMutations } from "../../src/api/exercises/mutations"
+import { usersResolvers } from "../../src/api/users/resolvers"
+import { usersMutations } from "../../src/api/users/mutations"
+import { roleResolvers } from "../../src/api/role/resolvers"
+import { roleMutations } from "../../src/api/role/mutations"
+import Users from "../../src/api/Exercises/Users.graphql"
+import Exercises from "../../src/api/exercises/Exercises.graphql"
+import Role from "../../src/api/role/Role.graphql"
+
+const resolvers = mergeResolvers([
+  usersResolvers,
+  usersMutations,
+  roleResolvers,
+  roleMutations,
+  exercisesResolvers,
+  exercisesMutations
+])
+
+const typeDefs = mergeTypeDefs([Exercises, Users, Role])
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers })
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+const server = apolloServer.createHandler({ path: "/api/graphql" })
+export default connectDb(server)
