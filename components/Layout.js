ca4c728bd84c6e851784121c59ce6a8f93ccbbf8
EricSSartorius <EricSSartorius@gmail.com>
2019-11-04 10:58:15 -0700

    Add styled components

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,18 @@
 import Head from "next/head"
 import Nav from "./nav"
+import GlobalStyles from "../styles/GlobalStyles"
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <>
+      <GlobalStyles />
       <Head>
         <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Nav />
       <main>{children}</main>
-    </div>
+    </>
   )
 }
 
---
+++ styles/GlobalStyles.js
@@ -0,0 +1,125 @@
+import { createGlobalStyle } from "styled-components"
+import vars from "./vars"
+
+const GlobalStyles = createGlobalStyle`
+  ${vars}
+
+  [data-theme="dark"] {
+    --bgColor: var(--blue-800);
+    --textColor: var(--white);
+    // --lineColor: var(--light-grey);
+  }
+
+  [data-theme="oled"] {
+    --bgColor: var(--black);
+    --textColor: var(--white);
+    --lineColor: var(--light-grey);
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  * {
+    min-height: 0;
+    min-width: 0;
+  }
+
+  html {
+    box-sizing: border-box;
+    font-family: var(--bodyFont);
+    font-display: swap;
+    font-size: 16px;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background: var(--bgColor);
+    color: var(--textColor);
+  }
+
+  #sapper {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+
+  #modal-root {
+    position: relative;
+    z-index: var(--highestLevel);
+  }
+
+  main,
+  #main {
+    overflow: hidden;
+    position: relative;
+    max-width: 56em;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-weight: bold;
+    font-family: var(--headingFont);
+    margin: var(--margin);
+    line-height: 1.15;
+    color: var(--textColor);
+    a {
+      color: var(--textColor);
+      text-decoration: none;
+    }
+  }
+
+  h1 {
+    color: var(--primaryColor);
+    font-size: var(--h-1);
+  }
+
+  h2 {
+    font-size: var(--h-2);
+  }
+
+  h3 {
+    font-size: var(--h-t3);
+  }
+
+  h4 {
+    font-size: var(--h-4);
+  }
+
+  h5 {
+    font-size: var(--h-5);
+  }
+
+  h6 {
+    font-size: var(--h-6);
+  }
+
+  p {
+    font-family: var(--bodyFont);
+    line-height: 36px;
+    margin: 1rem 0;
+    color: var(--textColor);
+  }
+
+  p.small {
+    font-size: var(--smallFontSize);
+  }
+
+  a {
+    color: var(--primaryColor);
+  }
+`
+
+export default GlobalStyles
