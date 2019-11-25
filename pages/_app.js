5819efd19fe49a821889dcbf925164275d7ecf97
EricSSartorius <EricSSartorius@gmail.com>
2019-11-25 12:23:39 -0700

    remove theme provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,15 @@
 import App from "next/app"
 import React from "react"
-import { ThemeProvider } from "styled-components"
 import GlobalStyles from "../styles/GlobalStyles"
-
-const theme = {
-  colors: {
-    primary: "#0070f3"
-  }
-}
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
     return (
-      <ThemeProvider theme={theme}>
+      <>
         <GlobalStyles />
         <Component {...pageProps} />
-      </ThemeProvider>
+      </>
     )
   }
 }
