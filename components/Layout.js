87e76e5638ac23ac9fda9d8da9d82f63092be5b2
EricSSartorius <EricSSartorius@gmail.com>
2019-11-25 12:22:17 -0700

    SSR styled components

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,10 @@
 import Head from "next/head"
 import Nav from "./nav"
 import Footer from "./Footer"
-import GlobalStyles from "../styles/GlobalStyles"
 
 const Layout = ({ children }) => {
   return (
     <>
-      <GlobalStyles />
       <Head>
         <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
